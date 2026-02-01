c9c042be7723a93a305ba3817fb10bb4f110ccc5
Zunuoh <audponu@gmail.com>
2026-02-01 23:18:23 +0000

    feat: integrate Storybook with Vitest and Playwright
    - Added Storybook configuration files and setup for React components.
    - Updated package.json to include Storybook and related dependencies.
    - Enhanced Button component to accept props and styled with Tailwind CSS.
    - Configured Vite to support Storybook testing with Vitest and Playwright.
    - Created stories for the Button component to demonstrate usage.
    - Added accessibility checks in Storybook preview configuration.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,10 @@
-export const Button = () => {
-    return <button>Button</button>
-}
+import type { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+
+export const Button = ({ ...props }: ButtonProps) => {
+  return (
+    <button className="text-blue-500" {...props}/>
+    
+  );
+};
