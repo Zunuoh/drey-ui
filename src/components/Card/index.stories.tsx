635144c403f5d67d85474d8f0b2902043cac07b6
Zunuoh <audponu@gmail.com>
2026-02-04 21:34:45 +0000

    added card

---
+++ src/components/Card/index.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { Card } from "."
+
+const meta: Meta<typeof Card> = {
+    title: "Components/Card",
+    component: Card,
+    parameters: {
+        layout: "centered"
+    },
+    tags: ["autodocs"]
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args:{
+        children: "card",
+        variant: "solid",
+        size: "md",
+        colorscheme: "primary"
+    }
+}
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,14 +1,36 @@
-import { cn } from "@/utils"
+import { cn } from "../../utils"
 import { cva, type VariantProps } from "class-variance-authority"
 import { forwardRef, type ComponentProps } from "react"
 
-const cardStyles = cva("w-full", {
+const cardStyles = cva(["w-full", "rounded-md"], {
     variants: {
+        variant: {
+        solid: "",
+        outline: "border-2",
+        ghost: "transition-colors duration-300",
+      },
         size: {
             sm: "h-5 px-2 py-2",
-            md: "h-10 px-4 py-4"
+            md: "h-10 px-4 py-4",
+            lg: "h-20 px-6 py-6",
+        },
+        colorscheme: {
+            primary: "text-white"
+        },
+    },
+    compoundVariants: [
+        {
+            variant: "solid",
+            colorscheme: "primary",
+            className: "text-white border-primary bg-primary border-4 flex justify-center items-center hover:bg-primary-600",
         }
-    }
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "md",
+      colorscheme: "primary",
+    },
+         
 }
     
 )
@@ -16,9 +38,9 @@ const cardStyles = cva("w-full", {
 type CardProps = ComponentProps<'div'> & VariantProps<typeof cardStyles>
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(
-    ({size, className, ...props}) => {
+    ({variant, size, colorscheme, className, ...props}, ref) => {
         return(
-            <div className={cn(cardStyles({size}))} {...props}/>
+            <div ref={ref} className={cn(cardStyles({variant, size, colorscheme, className}))} {...props}/>
         )
     }
 )
