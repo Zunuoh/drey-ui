19a9b444af9418c940b6a1bf724ce761fee0ac85
Zunuoh <audponu@gmail.com>
2026-02-09 21:16:37 +0000

    PolymorphicRef fixed

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,49 +1,77 @@
-import { cva, type VariantProps } from "class-variance-authority"
-import { forwardRef, type ComponentProps } from "react"
+import { cva, type VariantProps } from "class-variance-authority";
+import { forwardRef } from "react";
+import {
+  type PolymorphicComponentPropsWithRef,
+  type PolymorphicRef,
+} from "../../utils/types";
+import { cn } from "../../utils";
 
 const textStyles = cva("w-full", {
-    variants: {
-        emphasis: {
-            low: "text-gray-600 font-light"
-        },
-        size: {
-            sm: "text-sm",
-            base: "text-base",
-            lg: "text-lg",
-            xl: "text-xl",
-            "2xl": "text-2xl",
-            "3xl": "text-3xl",
-        },
-        weight: {
-        thin: "font-thin",
-        normal: "font-normal",
-        medium: "font-medium",
-        semibold: "font-semibold",
-        bold: "font-bold",
-        black: "font-black",
-        },
-        align: {
-        left: "text-left",
-        center: "text-center",
-        right: "text-right",
-        },
-        italic: {
-        true: "italic",
-        },
-        underline: {
-        true: "underline underline-offset-2",
-        },
+  variants: {
+    emphasis: {
+      low: "text-gray-600 font-light",
     },
-    defaultVariants: {
-        size: "base",
-        align: "left",
+    size: {
+      sm: "text-sm",
+      base: "text-base",
+      lg: "text-lg",
+      xl: "text-xl",
+      "2xl": "text-2xl",
+      "3xl": "text-3xl",
     },
-})
+    weight: {
+      thin: "font-thin",
+      normal: "font-normal",
+      medium: "font-medium",
+      semibold: "font-semibold",
+      bold: "font-bold",
+      black: "font-black",
+    },
+    align: {
+      left: "text-left",
+      center: "text-center",
+      right: "text-right",
+    },
+    italic: {
+      true: "italic",
+    },
+    underline: {
+      true: "underline underline-offset-2",
+    },
+  },
+  defaultVariants: {
+    size: "base",
+    align: "left",
+  },
+});
 
-type TextProps = ComponentProps<"span"> & VariantProps<typeof textStyles>;
+type TextProps<C extends React.ElementType> = PolymorphicComponentPropsWithRef<C, VariantProps<typeof textStyles>>;
 
-export const Text = forwardRef<HTMLSpanElement, TextProps>(
-   ({...props}, ref) => {
-    return <span ref={ref} {...props}/>
-   }
-);
+type TextComponent = <C extends React.ElementType = "span">(props: TextProps<C>) => React.ReactElement | null;
+
+const TextInner = <C extends React.ElementType = "span">(
+  { as, align, size, emphasis, italic, underline, weight, className, ...props }: TextProps<C>,
+  ref?: PolymorphicRef<C>
+) => {
+  const Component = as || "span";
+
+  return (
+    <Component
+      ref={ref}
+      className={cn(
+        textStyles({
+          size,
+          weight,
+          emphasis,
+          italic,
+          underline,
+          align,
+        }),
+        className
+      )}
+      {...props}
+    />
+  );
+};
+
+export const Text = forwardRef(TextInner) as TextComponent;
