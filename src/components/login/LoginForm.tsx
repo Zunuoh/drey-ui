7550dc1dc5ad0533d9416f2a59a114cf83cb0477
Zunuoh <audponu@gmail.com>
2026-02-09 20:37:11 +0000

    added a form

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,13 +1,18 @@
+import { Button } from "../Button"
 import { Input } from "../Input"
+import { Text } from "../Text"
 import { Box, Stack } from "../Layout"
 
 export const LoginForm = () => {
     return(
         <Box className="px-8 py-12 border border-gray-300 rounded-xl">
             <Stack>
-                <div>Login</div>
-                <div>Please enter your credentials</div>
+                <Text size="3xl" weight="bold" align="center">Login</Text>
+                <Text className="text-sm mb-4">Please enter your credentials</Text>
                 <Input type="text" placeholder="Username" id="username" className="mb-4"/>
+                <Input type="password" placeholder="Password" id="password" className="mb-4"/>
+                <Input type="password" placeholder="Confirm Password" id="confirmPassword" className="mb-4"/>
+                <Button variant="solid">Sign Up</Button>
             </Stack>
         </Box>
     )
