91a08f855e40d981bb3ffa9987dfb13f3e1d6e3c
Zunuoh <audponu@gmail.com>
2026-02-10 13:51:58 +0000

    fixed docs issue

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/react-vite",
--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,13 +1,34 @@
-import type {Meta, StoryObj} from "@storybook/react";
-import {Button} from './index';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./index";
+
 
 const meta: Meta<typeof Button> = {
-    title: "Components/Button",
-    component: Button,
-    parameters: {
-        layout: "centered"
-    },
-    tags: ["autodocs"]
+  title: "Components/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+    // docs: {
+    //     description: {
+    //         component: "A versatile button component with multiple variants and color schemes.",
+    //     },
+
+    // },
+  },
+  // argTypes: {
+  //     variant: {
+  //         control: "radio",
+  //         options: ["solid", "outline", "ghost"],
+  //     },
+  //     size: {
+  //         control: "radio",
+  //         options: ["sm", "md", "lg"],
+  //     },
+  //     colorscheme: {
+  //         control: "radio",
+  //         options: ["primary"],
+  //     },
+  // },
+  tags: ["autodocs"],
 };
 
 export default meta;
@@ -15,10 +36,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        children: "Click me",
-        variant: "solid",
-        size: "md",
-        colorscheme: "primary"
-    }
-}
+  args: {
+    children: "Click me",
+    variant: "solid",
+    size: "md",
+    colorscheme: "primary",
+  },
+};
